a18d9f9dd8c3e4b20efbac910f572f060b235744
phamcongthanhvn2k6 <phamcongt56@gmail.com>
2025-01-07 14:04:08 +0700

    Update session 16-bai1.cpp

--- session 16-bai1.cpp
+++ session 16-bai1.cpp
@@ -2,10 +2,13 @@
 
 int main(){
 	int a=10;
-	int *ptr;
+	int *ptr = &a;
 	ptr = &a;
-	printf("Gia tri cua Con tro: %d\n",*ptr);
-	printf("Dia chi cua con tro: %d\n",&ptr);
+	printf("Gia tri cua Con tro: theo cach 1: %d\n",a);
+	printf("Dia chi cua con tro theo cach 1: %d\n",&ptr);
+
+	printf("Gia tri cua Con tro: theo cach 2: %d\n",*ptr);
+	printf("Dia chi cua con tro theo cach 2: %d\n",(void*)ptr);
 	
 	return 0;
 }
